179484e50dc7279abfecd8a26c691ed219b4a1cd
Philip Dumaresq <phdumaresq@gmail.com>
2020-05-28 19:41:45 -0400

    Add files into BST to prioritize instead of a list. Added comments

--- src/index-project.lisp
+++ src/index-project.lisp
@@ -1,4 +1,11 @@
 (defun get-last-dir-in-path (path)
+  ":: pathname -> string
+
+  Takes a pathname, such as #P\"/usr/share/bin/\" and returns the 
+  last directory in that path name.
+
+  Note: Only works on directories, files will break it.
+  "
   (let* ((path-name (namestring path))
 	 (len-1 (- (length path-name) 1))
 	 (last-index (search (string *delimiter*)
@@ -8,12 +15,25 @@
     (subseq path-name (1+ last-index) len-1)))
 
 (defun index-directory (dir)
+  ":: string -> void
+
+  Takes a directory and loops over all files and subdirectories.
+    - Files are added to a BST sorted by size.
+    - Directories are passed to this function in a recursive call.
+  "
   (let ((files (uiop:directory-files dir))
-	(subdirs (uiop:subdirectories dir)))
-    (setf *files* (concatenate 'vector *files* files))
+        (subdirs (uiop:subdirectories dir))
+        (bst:*bst-copy-function* #'copy-seq)
+        (bst:*bst-equal-p-function* #'util:file=)
+        (bst:*bst-lesser-p-function* #'util:file<))
+    ; Add files to BST sorted by size
+    (loop for file in files do
+      (setf *files* (bst:bst-add! *files* `(:name ,file
+                                            :size ,(util:file-size file)))))
+    ; Loop through directories and call recursively
     (when subdirs
       (loop for dir in subdirs 
-	    when (not (find (get-last-dir-in-path dir)
-			    *ignore*
-			    :test #'string=)) do
-	(index-directory dir)))))
+            when (not (find (get-last-dir-in-path dir)
+                            *ignore-dirs*
+                            :test #'string=)) do
+        (index-directory dir)))))
