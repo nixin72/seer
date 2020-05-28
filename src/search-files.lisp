9e23b9ea52b800e7bb0bba365c2d3459e604dcb5
Philip Dumaresq <phdumaresq@gmail.com>
2020-05-28 19:43:50 -0400

    Searching files beginning with an initial character and find all files with that char

--- src/search-files.lisp
+++ src/search-files.lisp
@@ -1,16 +1,29 @@
-(defparameter *matches* '(head))
+(defparameter *matches* '())
 
 (defmacro append! (list element)
   `(setf ,list (append ,list (list ,element))))
 
 (defun search-for-initial-matches (filename search)
+  "
+  Given a beginning character, find all lines in the file that 
+  contain that character.
+  "
   (with-open-file (stream filename)
     (loop for line = (read-line stream nil)
-	  for i from 1 do
-      (loop for char across line do
-	(when (eq char search)
-	  (append *matches* `(:file ,filename :row i :line line :match))
-	  )))))
+          for linum from 1
+          until (eq line nil) do
+      (loop for char across line
+            for col from 1 do
+        (when (eq char search)
+          (append! *matches* `(:file ,filename
+                               :linum ,linum
+                               :col ,col
+                               :line ,line
+                               :match))
+        )))))
 
-(defmacro define (def body)
-  `(defun ,(car def) ,(cdr def) ,body))
+(defun search-files (search)
+  (bst:in-order-traversal
+   *files*
+   #'(lambda (file)
+       (search-for-initial-matches file search))))
